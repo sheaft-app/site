93a9e4813400132a1c2f5e4bef973ced765d295f
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2020-05-26 19:23:58 +0200

    Fix - Oubli bouton nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -96,14 +96,14 @@
           </a>
         </li>
       </ul>
-      <a
+      <!-- <a
         id="navAction"
         class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold
         rounded-full mt-4 lg:mt-0 py-4 px-8 shadow"
         target="_blank"
         href="https://app.sheaft.com">
         Ouvrir Sheaft
-      </a>
+      </a> -->
     </div>
   </div>
 </nav>
