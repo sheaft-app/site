935be1797e346417f02d91a5b569b3925067ab60
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2020-10-21 16:03:00 +0200

    New - Ajout d'une page "intéressé" pour le mailing producteur

---
+++ src/routes/interest.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <title>Merci de votre intérêt !</title>
+</svelte:head>
+
+<div class="flex flex-wrap justify-center px-12 py-20 bg-white items-center">
+  <div class="w-full md:w-6/12 text-center items-center">
+    <img class="pb-12 md:p-16"
+      src="/img/interested.svg"
+      height="200"
+      style="height: 500px;"
+      alt="newsletter" />
+  </div>
+  <div class="text-2xl text-center">
+    <p>Merci de votre intérêt pour Sheaft, nous reviendrons vers vous sous peu. 😊</p>
+    <p>Si vous avez la moindre question d'ici là, n'hésitez pas à nous envoyer un mail sur <b>contact@sheaft.com</b></p>
+  </div>
+</div>
